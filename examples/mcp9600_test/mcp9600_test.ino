9f2675f6394ea7924723c3ceccf380a57efe5e8f
ladyada <limor@ladyada.net>
2019-04-05 01:41:27 -0400

    demo

--- examples/mcp9600_test/mcp9600_test.ino
+++ examples/mcp9600_test/mcp9600_test.ino
@@ -8,10 +8,9 @@
 Adafruit_MCP9600 mcp;
 Adafruit_I2CDevice i2c_dev = Adafruit_I2CDevice(I2C_ADDRESS);
 
-void
-setup()
+void setup()
 {
-    Serial.begin(11520);
+    Serial.begin(115200);
     while (!Serial) {
       delay(10);
     }
@@ -23,12 +22,50 @@ setup()
         while (1);
     }
 
-    Serial.println("Found MCP9600!");
+  Serial.println("Found MCP9600!");
+
+  mcp.setADCresolution(MCP9600_ADCRESOLUTION_18);
+  Serial.print("ADC resolution set to ");
+  switch (mcp.getADCresolution()) {
+    case MCP9600_ADCRESOLUTION_18:   Serial.print("18"); break;
+    case MCP9600_ADCRESOLUTION_16:   Serial.print("16"); break;
+    case MCP9600_ADCRESOLUTION_14:   Serial.print("14"); break;
+    case MCP9600_ADCRESOLUTION_12:   Serial.print("12"); break;
+  }
+  Serial.println(" bits");
+
+  mcp.setThermocoupleType(MCP9600_TYPE_K);
+  Serial.print("Thermocouple type set to ");
+  switch (mcp.getThermocoupleType()) {
+    case MCP9600_TYPE_K:  Serial.print("K"); break;
+    case MCP9600_TYPE_J:  Serial.print("J"); break;
+    case MCP9600_TYPE_T:  Serial.print("T"); break;
+    case MCP9600_TYPE_N:  Serial.print("N"); break;
+    case MCP9600_TYPE_S:  Serial.print("S"); break;
+    case MCP9600_TYPE_E:  Serial.print("E"); break;
+    case MCP9600_TYPE_B:  Serial.print("B"); break;
+    case MCP9600_TYPE_R:  Serial.print("R"); break;
+  }
+  Serial.println(" type");
+
+  mcp.setFilterCoefficient(3);
+  Serial.print("Filter coefficient value set to: ");
+  Serial.println(mcp.getFilterCoefficient());
+
+  mcp.setAlertTemperature(1, 30);
+  Serial.print("Alert #1 temperature set to ");
+  Serial.println(mcp.getAlertTemperature(1));
+  mcp.configureAlert(1, true, true);  // alert 1 enabled, rising temp
+
+  mcp.enable(true);
+
+  Serial.println(F("------------------------------"));
 }
 
-void
-loop()
+void loop()
 {
-    Serial.println("ToDo");
-    delay(1000);
+  Serial.print("Hot Junction: "); Serial.println(mcp.readThermocouple());
+  Serial.print("Cold Junction: "); Serial.println(mcp.readAmbient());
+  Serial.print("ADC: "); Serial.print(mcp.readADC() * 2); Serial.println(" uV");
+  delay(1000);
 }
